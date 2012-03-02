90245dafa5e909b52b0bc88ae55c902c06def589
Bill Dimmick <me@billdimmick.com>
2012-03-01 22:40:05 -0800

    Commit of trie module, unit tests, sample code, license, and readme

---
+++ sample.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python -B
+
+from trie import Trie
+
+""" Here's a couple of helper methods to print on assigment and dump the trie.
+    You don't need in practice and can just treat the Trie as any other dict-like
+	structure.
+"""
+def assign(t, k, v):
+	print "Assigning %s => t[%s]" % (v, k)
+	t[k]=v
+
+def dump(t):
+	print "Dumping trie:"
+	for k in t.keys():
+		print "  t[%s] => %s" % (k, t[k])
+
+
+"""  Real Samples Start Here """
+
+print "\nUsing a simple string as keys and numeric values..."
+t = Trie()
+assign(t, 'string1', 1)
+assign(t, 'string2', 2)
+dump(t)
+
+print "\nUsing lists as keys and bool values..."
+t = Trie()
+assign(t, [1,2], True)
+assign(t, [2,3], False)
+dump(t)
+
+print "\nUsing mixed types as keys and values..."
+t = Trie()
+assign(t, [1,2], 'Hello?')
+assign(t, 'World', 'Earth')
+assign(t, 'Planet Number', 3)
+assign(t, (2,1), 'X')
+dump(t)
---
+++ trie.py
@@ -0,0 +1,109 @@
+class Trie:
+	
+	def __init__(self):
+		self.path={}
+		self.value=None
+	
+	def __setitem__(self, key, value):
+		head = key[0]
+		if self.path.has_key(head):
+			node = self.path[head]
+		else:
+			node = Trie()
+			self.path[head] = node
+		
+		if len(key) > 1:
+			remains = key[1:]
+			node.__setitem__(remains, value)
+		else:
+			node.value = value
+
+	def __delitem__(self, key):
+		head = key[0]
+		if self.path.has_key(head):
+			node = self.path[head]
+			if len(key) > 1:
+				remains = key[1:]
+				node.__delitem__(remains)
+			else:
+				node.value = None 
+			if len(node) == 0:
+				del self.path[head]
+
+	def __getitem__(self, key):
+		head = key[0]
+		if self.path.has_key(head):
+			node = self.path[head]
+		else:
+			return None 
+		if len(key) > 1:
+			remains = key[1:]
+			return node.__getitem__(remains)
+		else:
+			return node.value
+
+	def __contains__(self, key):
+		return not self.__getitem__(key)==None
+
+	def __len__(self):
+		n = (self.value and 1) or 0
+		for k in self.path.iterkeys():
+			n = n + len(self.path[k])
+		return n
+
+	def nodeCount(self):
+		n = 0
+		for k in self.path.iterkeys():
+			n = n + 1 + self.path[k].nodeCount()
+		return n
+
+	def keys(self, prefix=[]):
+		result = []
+		if self.value != None:
+			isStr = True
+			val = ""
+			for k in prefix:
+				if type(k)!=str or len(k)>2:
+					isStr = False
+					break
+				else:
+					val+=k
+			if isStr:
+				result.append(val)	
+			else:
+				result.append(prefix)
+		for k in self.path.iterkeys():
+			next = []
+			next.extend(prefix)
+			next.append(k)
+			result.extend(self.path[k].keys(next))
+		return result
+
+	def __iter__(self):
+		for k in self.keys():
+			yield k
+		raise StopIteration
+
+
+	def __add__(self, other):
+		result = Trie()
+		result+=self
+		result+=other
+		return result
+	
+	def __sub__(self, other):
+		result = Trie()
+		result+=self
+		result-=other
+		return result
+
+	def __iadd__(self, other):
+		for k in other:
+			self[k]=other[k]
+		return self
+
+	def __isub__(self, other):
+		for k in other:
+			del self[k]
+		return self
+
